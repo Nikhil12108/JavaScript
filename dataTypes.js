9144eae792b5966f150d0019543b17ffaa27afd2
Nikhil Kumar <77448414+Nikhil12108@users.noreply.github.com>
2024-08-24 07:47:54 +0000

    added data types

---
+++ dataTypes.js
@@ -0,0 +1,27 @@
+//"use strict" this line will make engine treat code as newer js code
+
+//alert("hello") we are  using nodejs not browser so it cannot  be used here there is a different syntax for it here will discuss later
+
+// code readability and future scope is at most priority while writing industry level Code
+
+// MDN doc is the easiest to read but original documentation is of ECAM International, prefer MDN
+//LINK: https://developer.mozilla.org/en-US/
+
+let name = "Nikhi;" //String
+let age = 23 // Number
+let isLoggedIn = false; //Boolean
+
+//Number
+//range is 2^53 approx
+//bitint, not used often
+
+// null, it is a stand alone value
+let state; // undefined
+let state1 = null // not value present but not undefined
+
+
+// symbol
+/*In JavaScript, Symbol is a unique and immutable data type that was introduced in ECMAScript 6 (ES6). It is used primarily for creating unique identifiers for object properties, which helps in avoiding name collisions in objects, especially when working with multiple libraries or codebases.*/
+
+
+// object
