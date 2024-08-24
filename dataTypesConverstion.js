b00a0076d633d301b5497dca4f33c5e40cec0609
Nikhil Kumar <77448414+Nikhil12108@users.noreply.github.com>
2024-08-24 12:39:51 +0000

    added data types conversions

---
+++ dataTypesConverstion.js
@@ -0,0 +1,47 @@
+let score = 50
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+// answer = number
+
+let score1 = "50"
+
+//console.log(typeof score1);
+//console.log(typeof(score1));
+
+//answer = String
+
+// converting this string to number
+let Converted = Number(score1)
+
+//console.log(typeof Converted)
+//answer  = number
+
+let variable = "123ABC";
+variable = Number(variable);
+console.log(typeof variable)
+//answer = number
+
+console.log(variable);
+// answer = NaN
+
+
+// "33" => 33
+// "33abc" => NaN, type of Nan is a number
+// true => 1, false => 0
+
+
+let isLoggedin = 1;
+let booleanIsLoggedIn = Boolean(isLoggedin);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn)
+
+// comparisions
+console.log(null> 0 ) // false
+console.log(null == 0) // false
+console.log(null >= 0) // true
+
+// undefined will always be false
+
+
