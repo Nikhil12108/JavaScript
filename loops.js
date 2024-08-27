258270ed9e76e9db00dfe25915564092d45b1f0e
Nikhil Kumar <77448414+Nikhil12108@users.noreply.github.com>
2024-08-27 10:30:20 +0000

    added dom and loops with one minor project color changer

---
+++ loops.js
@@ -0,0 +1,16 @@
+// for of loop
+["", "", ""]
+
+[{}, {}, {}]
+
+const array = [1, 2, 3, 4, 5]
+
+for (const element of array) {
+    console.log(element)
+}
+
+const name = "NIKHIL"
+
+for (const element of name) {
+    console.log(element)
+}
---
+++ practice Projects/color_changer/index.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', function(event) {
+        console.log(event);
+        console.log(event.target) // where is the event coming from
+        if(event.target.id == "grey") {
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id == "white") {
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id == "blue") {
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id == "yellow") {
+            body.style.backgroundColor = event.target.id
+        }
+    })
+})
